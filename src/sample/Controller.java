28f70182b4f4b93ed2dea7da0b100b979d85b27b
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-11-07 12:03:24 +0100

    OpenURL

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,17 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
+import java.awt.desktop.OpenURIHandler;
+import java.net.URL;
+
+import java.awt.*;
+import java.awt.desktop.OpenFilesEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class Controller {
 @FXML
@@ -31,5 +41,11 @@ TextArea outputDisplay;
     public void getInfo(ActionEvent actionEvent) {
 
         outputDisplay.setText(outputDisplay.getText() + "Navn: Dan" + "\nAlder: 21 år" + "\nUddannelse: DAT\n");
+    }
+
+    public void openTank(MouseEvent mouseEvent) throws IOException {
+        String htmlPath = "www.tanktrouble.com";
+        java.awt.Desktop.getDesktop().browse(java.net.URI.create(htmlPath));
+
     }
 }
